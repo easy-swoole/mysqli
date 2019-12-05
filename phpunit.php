1d5440d5fccdb52eff42ca735006d76446e43aa4
宣言 <59419979@qq.com>
2019-12-05 16:10:07 +0800

    insertAll

--- phpunit.php
+++ phpunit.php
@@ -3,9 +3,9 @@
 defined("MYSQL_CONFIG") ?: define('MYSQL_CONFIG', [
     'host'          => '127.0.0.1',
     'port'          => 3306,
-    'user'          => 'demo',
-    'password'      => '123456',
-    'database'      => 'demo',
+    'user'          => 'root',
+    'password'      => 'root',
+    'database'      => 'employees',
     'timeout'       => 5,
     'charset'       => 'utf8mb4',
 ]);
