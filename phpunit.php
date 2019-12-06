cc6a144374bf9a2baa2a5ff1a03dece34a82c2db
f2h2h1 <h5e4g2l6e1g0a0a8@protonmail.com>
2019-12-06 14:50:10 +0800

    添加用于测试的 sql

--- phpunit.php
+++ phpunit.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * -- 新建测试用的库
+ * CREATE DATABASE IF NOT EXISTS demo
+ * DEFAULT CHARACTER SET utf8mb4
+ * DEFAULT COLLATE utf8mb4_general_ci;
+ *
+ * -- 新建测试用的表
+ * CREATE TABLE demo.user_test_list (
+ *     name varchar(64) NULL,
+ *     age TINYINT UNSIGNED NULL,
+ *     addTime DATETIME NULL,
+ *     state TINYINT UNSIGNED NULL
+ * )
+ * ENGINE=InnoDB
+ * DEFAULT CHARSET=utf8mb4
+ * COLLATE=utf8mb4_general_ci;
+ */
+
 defined("MYSQL_CONFIG") ?: define('MYSQL_CONFIG', [
     'host'          => '127.0.0.1',
     'port'          => 3306,
