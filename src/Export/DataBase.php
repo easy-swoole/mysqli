df3eb80209968d5b8c60108ec4ae7ae265c66568
gaobinzhan <gaobinzhan@gmail.com>
2020-08-04 16:15:11 +0800

    export

--- src/Export/DataBase.php
+++ src/Export/DataBase.php
@@ -10,19 +10,43 @@ class DataBase
 {
     protected $client;
 
-    function __construct(Client  $client)
+    function __construct(Client $client)
     {
         $this->client = $client;
     }
 
-    function showTables():array
+    function showTables(): array
     {
-
+        $tables = [];
+        $tableNames = $this->client->rawQuery('SHOW TABLES;');
+        foreach ($tableNames as $tableName) {
+            $tables[] = new Table($this->client, current($tableName));
+        }
+        return $tables;
     }
 
 
-    function export(&$output,bool $onlyStruct = false)
+    function export(&$output, bool $onlyStruct = false)
     {
+        $tables = $this->showTables();
 
+        $serverInfo = $this->client->mysqlClient()->serverInfo;
+        $version = current(current($this->client->rawQuery('SELECT VERSION();')));
+        $front = '-- EasySwoole MySQL dump, for ' . PHP_OS . PHP_EOL;
+        $front .= '--' . PHP_EOL;
+        $front .= "-- Host: {$serverInfo['host']}    Database: {$serverInfo['database']}" . PHP_EOL;
+        $front .= '-- ------------------------------------------------------' . PHP_EOL;
+        $front .= "-- Server version	{$version}" . PHP_EOL . PHP_EOL;
+
+        if (is_resource($output)) {
+            fwrite($output, $front);
+        } else {
+            $output = $front;
+        }
+
+        foreach ($tables as $table) {
+            if (!$table instanceof Table) continue;
+            $table->export($output, $onlyStruct);
+        }
     }
 }
--- src/Export/Table.php
+++ src/Export/Table.php
@@ -5,29 +5,106 @@ namespace EasySwoole\Mysqli\Export;
 
 use EasySwoole\DDL\Blueprint\Table as DDLTable;
 use EasySwoole\Mysqli\Client;
+use EasySwoole\Mysqli\QueryBuilder;
 
 class Table
 {
     protected $client;
     protected $tableName;
-    function __construct(Client $client,string $tableName)
+
+    function __construct(Client $client, string $tableName)
     {
         $this->client = $client;
         $this->tableName = $tableName;
     }
 
-    function ddl():DDLTable
+    /*function ddl(): DDLTable
     {
+
+    }*/
+
+    function ddl()
+    {
+        $result = $this->client->rawQuery("SHOW CREATE TABLE `{$this->tableName}`");
+        return $result[0]['Create Table'];
+    }
+
+
+    function export(&$output, bool $onlyStruct = false)
+    {
+        $structure = '--' . PHP_EOL;
+        $structure .= "-- Table structure for table `{$this->tableName}`" . PHP_EOL;
+        $structure .= '--' . PHP_EOL . PHP_EOL;
+        $structure .= "DROP TABLE IF EXISTS `{$this->tableName}`;" . PHP_EOL;
+        $structure .= $this->ddl() . ';' . PHP_EOL . PHP_EOL;
+
+
+        if (is_resource($output)) {
+            fwrite($output, $structure);
+        } else {
+            $output = $structure;
+        }
+
+        if ($onlyStruct) return;
+
+        $data = '--' . PHP_EOL;
+        $data .= "-- Dumping data for table `{$this->tableName}`" . PHP_EOL;
+        $data .= '--' . PHP_EOL . PHP_EOL;
+        $data .= "LOCK TABLES `{$this->tableName}` WRITE;" . PHP_EOL;
+
+        if (is_resource($output)) {
+            fwrite($output, $data);
+        } else {
+            $output = $data;
+        }
+
+        $page = 1;
+        $size = 1000;
+        while (true) {
+            $insertSql = $this->getInsertSql($page, $size);
+            if (empty($insertSql)) break;
+
+            if (is_resource($output)) {
+                fwrite($output, $insertSql);
+            } else {
+                $output = $insertSql;
+            }
+
+            $page++;
+        }
+
+        $data = 'UNLOCK TABLES;' . PHP_EOL . PHP_EOL;
+
+        if (is_resource($output)) {
+            fwrite($output, $data);
+        } else {
+            $output = $data;
+        }
 
     }
 
-    function export(&$output,bool $onlyStruct = false)
+    private function getInsertSql($page = 1, $size = 1000): string
     {
-        $result = '';
-        if(is_resource($output)){
-            fwrite($output,$result);
-        }else{
-            $output = $result;
+        $limit = ($page - 1) * $size;
+        $this->client->queryBuilder()->limit($limit, $size)->get($this->tableName);
+        $result = $this->client->execBuilder();
+        $queryBuilder = new QueryBuilder();
+
+        $data = '';
+        foreach ($result as $item) {
+
+          /*  $item = array_map(function ($v) {
+                return str_replace(["\r", "\n", "'", '"'], ['\r', '\n', "\'", '\"'], $v);
+            }, $item);*/
+
+
+            $queryBuilder->insert($this->tableName, $item);
+            $insertSql = $queryBuilder->getLastQuery();
+            $queryBuilder->reset();
+
+            $data .= "{$insertSql};" . PHP_EOL;
         }
+
+        return $data ?? '';
     }
 }
