76a0e49f02f932deb218f6a75c8edbfa8d1db9df
gaobinzhan <gaobinzhan@gmail.com>
2020-08-20 21:40:15 +0800

    testing

---
+++ tests/DatabaseTest.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * @author gaobinzhan <gaobinzhan@gmail.com>
+ */
+
+
+namespace EasySwoole\Mysqli\Tests;
+
+
+use EasySwoole\Mysqli\Export\Config;
+use EasySwoole\Mysqli\Export\DataBase;
+use EasySwoole\Mysqli\Export\Event;
+use PHPUnit\Framework\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    protected $client;
+
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        $config = new \EasySwoole\Mysqli\Config();
+        $config->setHost('127.0.0.1');
+        $config->setUser('root');
+        $config->setPort(3306);
+        $config->setPassword('gaobinzhan');
+        $config->setDatabase('blog');
+        $config->setTimeout(-1);
+
+        $this->client = new \EasySwoole\Mysqli\Client($config);
+
+        parent::__construct($name, $data, $dataName);
+    }
+
+    public function testExport()
+    {
+        $config = new Config();
+        $config->setInTable(['test']);
+        $config->setSize(2000);
+        $database = new DataBase($this->client, $config);
+
+        $fp = fopen('./test.sql', 'w+');
+        $database->export($fp);
+        $this->assertTrue(file_exists('./test.sql'));
+    }
+
+    public function testImport()
+    {
+        $config = new Config();
+        $config->setSize(20000);
+        $database = new DataBase($this->client, $config);
+        $result = $database->import('./test.sql', 'r+');
+        $this->assertEmpty($result->getErrorSqls());
+        $this->assertEmpty($result->getErrorMsgs());
+    }
+
+    public function testProcessBar()
+    {
+        $config = new Config();
+        $config->setCallback(\EasySwoole\Mysqli\Export\Event::onBeforeExportTableData, function (\EasySwoole\Mysqli\Client $client, $tableName) {
+            echo "Dumping data for table `{$tableName}`" . PHP_EOL;
+            return current(current($client->rawQuery("SELECT COUNT(1) FROM {$tableName}")));
+        });
+
+        $config->setCallback(\EasySwoole\Mysqli\Export\Event::onExportingTableData, function (\EasySwoole\Mysqli\Client $client, $tableName, $page, $size, $totalCount) {
+            $currentCount = ($page * $size) > $totalCount ? $totalCount : ($page * $size);
+            printf("[%-100s] %d%% \r", str_repeat('=', $currentCount / $totalCount * 100) . '>', $currentCount / $totalCount * 100);
+        });
+
+        $config->setCallback(\EasySwoole\Mysqli\Export\Event::onAfterExportTableData, function () {
+            echo PHP_EOL;
+        });
+
+        $config->setCallback(\EasySwoole\Mysqli\Export\Event::onBeforeImportTableData, function (\EasySwoole\Mysqli\Client $client, $resource) {
+            $totalLine = 0;
+            while (!feof($resource)) {
+                fgets($resource);
+                $totalLine++;
+            }
+            // 倒回文件指针的位置
+            if (!rewind($resource)) {
+                throw new \EasySwoole\Mysqli\Exception\DumpException('Failed to reset file pointer position');
+            }
+
+            return $totalLine;
+        });
+
+        $currentLine = 0;
+        $config->setCallback(\EasySwoole\Mysqli\Export\Event::onImportingTableData, function (\EasySwoole\Mysqli\Client $client, $totalCount) use (&$currentLine) {
+            $currentCount = ++$currentLine;
+            printf("[%-100s] %d%% \r", str_repeat('=', $currentCount / $totalCount * 100) . '>', $currentCount / $totalCount * 100);
+        });
+
+        $config->setCallback(\EasySwoole\Mysqli\Export\Event::onAfterImportTableData, function (\EasySwoole\Mysqli\Client $client) {
+            echo PHP_EOL;
+        });
+
+
+        $config->setInTable(['test']);
+        $config->setSize(3000);
+        $database = new DataBase($this->client, $config);
+        $fp = fopen('./test.sql', 'w+');
+        $database->export($fp);
+        $this->assertTrue(file_exists('./test.sql'));
+
+        $result = $database->import('./test.sql');
+        $this->assertEmpty($result->getErrorSqls());
+        $this->assertEmpty($result->getErrorMsgs());
+    }
+
+
+    public function testEvent()
+    {
+        $config = new Config();
+        $config->setCallback(Event::onWriteFrontNotes, function () {
+            return 'This is EasySwoole Mysqli Dump';
+        });
+
+        $config->setCallback(Event::onWriteTableStruct, function () {
+            return 'Table Struct';
+        });
+
+        $config->setCallback(Event::onBeforeWriteTableDataNotes, function () {
+            return 'Before';
+        });
+
+        $config->setCallback(Event::onAfterWriteTableDataNotes, function () {
+            return 'After';
+        });
+
+        $config->setCallback(Event::onWriteCompletedNotes, function () {
+            return 'Success';
+        });
+
+        $config->setInTable(['blog_users']);
+        $database = new DataBase($this->client, $config);
+        $fp = fopen('./test111.sql', 'w+');
+        $database->export($fp);
+        $this->assertContains('This is EasySwoole Mysqli Dump', file_get_contents('./test111.sql'));
+        $this->assertContains('Table Struct', file_get_contents('./test111.sql'));
+        $this->assertContains('Before', file_get_contents('./test111.sql'));
+        $this->assertContains('After', file_get_contents('./test111.sql'));
+        $this->assertContains('Success', file_get_contents('./test111.sql'));
+    }
+
+    public function testOptimize()
+    {
+        $database = new DataBase($this->client);
+        $result = $database->optimize(['test']);
+        $this->assertIsArray($result);
+    }
+
+    public function testRepair()
+    {
+        $database = new DataBase($this->client);
+        $result = $database->repair(['test']);
+        $this->assertIsArray($result);
+    }
+}
